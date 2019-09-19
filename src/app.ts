779483ba47a361611fabecc827bc59d6ae916f63
MaarySantos <maary.santos1@gmail.com>
2019-09-19 14:48:14 -0300

    Add express, cors

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express'
+import cors from 'cors'
+
+class App {
+    public express: express.Application
+    constructor () {
+      this.express = express()
+      this.middlewares()
+      this.routes()
+    }
+
+    private middlewares (): void {
+      this.express.use(express.json())
+      this.express.use(cors())
+    }
+
+    private routes (): void {
+      this.express.get('/', (req, res) => {
+        return res.send('Hello')
+      })
+    }
+}
+
+export default new App().express
